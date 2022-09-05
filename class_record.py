fcee12d62f005c99940fb36535960c4f40adf7ff
DanyaSH <97675611+DanyaSH759@users.noreply.github.com>
2022-09-06 01:44:59 +0300

    Update class_record.py

--- class_record.py
+++ class_record.py
@@ -5,19 +5,32 @@ class Record:
     #класс записи и использования словаря
 
     def take_data(self):
-    #функция по вводу данных и проверки на выход из цикла
-        self.word = input('введите слово:')
-        self.translate = input('введите перевод:')
+    #функция по вводу данных и проверка на остановку запист слов
+        print('Для остановки записи введите \'stop\'')
+
+        w1 = input('введите слово:')
+        if w1 == 'stop':
+            pass
+        else:
+            self.word = w1
+
+        w2 = input('введите перевод:')
+        if w2 == 'stop':
+            pass
+        else:
+            self.translate = w2
+
 
     def write_date_eng(self):
+        #устанавливает соеденение с бд
         a = sql()
         a.connections()
         sqltext = "INSERT INTO words_eng(word, translate) VALUES (?,?);"
         data_tuple = (self.word, self.translate)
-        a.curs.execute(sqltext, data_tuple)
-        a.db.commit()
+        a.curs.execute(sqltext, data_tuple) #загружает полученные слова в указанные выше колонки
+        a.db.commit() #передача данных (закрытие транзакции)
         print('данные вставлены в таблицу')
-        a.conn_close()
+        a.conn_close() #закрытие соеденения
 
 
 
