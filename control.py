7aa4aa0f6319f5ea7f2953c632ff9c3f4009c6b8
DanyaSH <97675611+DanyaSH759@users.noreply.github.com>
2022-09-06 01:44:14 +0300

    Update control.py

--- control.py
+++ control.py
@@ -1,15 +1,25 @@
 from class_record import *
-from Class_sql import *
+
+from Game import *
 
 def record_words():
-    # Функция для создания цикла записи слов словарь
-    rec = Record()
-    rec.take_data()
-    rec.write_date_eng()
+    # Функция для создания цикла записи слов в словарь
+    try:
+        rec = Record()
+        rec.take_data()
+        rec.write_date_eng()
+    except AttributeError:
+        pass
 
 def show():
+    #функция отображает все слова записанные в таблице
     sh = sql()
     sh.show_eng_words()
+
+def start_game():
+    #функция по запуску игры
+    game = Game()
+    game.game_eng()
 
 if __name__ == '__main__':
     print('Словарь активирован')
@@ -17,9 +27,11 @@ if __name__ == '__main__':
           '\nВведите \'start\' что бы начать запись слов в словарь'
           '\nВведите / что бы повторно активировать режим записи слов'
           '\nВведите \'show\' что просмотреть все слова в словаре'
-          '\nВведите \'ex_all\' что бы закрыть словарь')
+          '\nВведите \'exit\' что бы закрыть словарь'
+          '\nВведите \'game\' что бы начать игру')
 
     while True:
+        console = [{'script': 'd.py'}]
         z = input('>>>')
         if z == 'start' or z == '/':
             record_words()
@@ -27,7 +39,10 @@ if __name__ == '__main__':
         elif z == 'show':
             show()
 
-        elif z == 'exit' or z == 'ex_all':
+        elif z == 'game':
+            start_game()
+
+        elif z == 'exit':
             break
 
 
